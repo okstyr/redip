a5c77a1421dfbb2835a73a7a35fdda945a5b7bb7
amuhrer <adrian.muhrer@rea-group.com>
2013-04-19 19:49:41 +1000

    basic movement example

--- src/orders.py
+++ src/orders.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 import yaml
 
-class MoveException(Exception):
-    pass
-
 class Territory:
     def __init__(self, name, neighbours):
         self.name = name
@@ -23,20 +20,12 @@ class World:
     def __repr__(self):
         return "World: " + " ".join(map(lambda x: str(x), self.territories.values()))
 
-    def move(self, src, dest):
-        start = self.territories[src]
-        if dest not in start.neighbours:
-            raise MoveException('Cannot move from %s to %s' % (src, dest))
-        print 'yay'
-
 
 def parse():
     with open('fixtures/3_territories.yml', 'r') as f:
         yml = yaml.load(f)
 
     w = World(yml)
-    w.move('rom', 'tus')
-    w.move('rom', 'pie')
 
     print w
 
